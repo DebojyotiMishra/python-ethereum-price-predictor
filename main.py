6093c816bf6f3d4a6772615dda1e1f473618237c
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2024-04-02 15:41:08 +0200

    comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Importing necessary libraries
 import pandas as pd
 import yfinance as yf
 from datetime import datetime
@@ -7,46 +8,68 @@ from prophet import Prophet
 from prophet.plot import plot_plotly, plot_components_plotly
 import warnings
 
+# Ignoring warnings
 warnings.filterwarnings('ignore')
 
+# Setting display format for float values
 pd.options.display.float_format = '${:,.2f}'.format
 
+# Getting today's date and start date for data retrieval
 today = datetime.today().strftime('%Y-%m-%d')
 start_date = '2016-01-01'
 
-eth_df = yf.download('ETH-USD',start_date, today)
+# Downloading Ethereum price data from Yahoo Finance
+eth_df = yf.download('ETH-USD', start_date, today)
 
+# Displaying the last few rows of the dataframe
 eth_df.tail()
+
+# Displaying information about the dataframe
 eth_df.info()
+
+# Checking for missing values in the dataframe
 eth_df.isnull().sum()
+
+# Getting the column names of the dataframe
 eth_df.columns
+
+# Resetting the index of the dataframe
 eth_df.reset_index(inplace=True)
+
+# Getting the column names of the dataframe after resetting the index
 eth_df.columns 
 
+# Creating a new dataframe with only the "Date" and "Open" columns
 df = eth_df[["Date", "Open"]]
 
+# Renaming the columns of the dataframe
 new_names = {
     "Date": "ds", 
     "Open": "y",
 }
-
 df.rename(columns=new_names, inplace=True)
+
+# Displaying the last few rows of the modified dataframe
 df.tail()
 
-
 # ================================ Time Series Plot for Ethereum Open Prices ================================
+
+# Extracting the x and y values for the plot
 x = df["ds"]
 y = df["y"]
 
+# Creating a new plot figure
 fig = go.Figure()
 
+# Adding a scatter trace to the plot
 fig.add_trace(go.Scatter(x=x, y=y))
 
-# Set title
+# Setting the title of the plot
 fig.update_layout(
     title_text="Time series plot of Ethereum Open Price",
 )
 
+# Adding a range selector and rangeslider to the x-axis of the plot
 fig.update_layout(
     xaxis=dict(
         rangeselector=dict(
@@ -66,22 +89,29 @@ fig.update_layout(
 )
 
 # ================================ Prophet Model ================================
+
+# Creating a Prophet model with multiplicative seasonality
 m = Prophet(
     seasonality_mode="multiplicative" 
 )
 
+# Fitting the model to the data
 m.fit(df)
 
-future = m.make_future_dataframe(periods = 365)
+# Creating a dataframe for future predictions
+future = m.make_future_dataframe(periods=365)
 future.tail()
 
+# Generating predictions using the trained model
 forecast = m.predict(future)
 forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 
-# Price preditction for the next day
+# Predicting the price for the next day
 next_day = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')
 forecast[forecast['ds'] == next_day]['yhat'].item()
 
+# Plotting the forecasted values
 plot_plotly(m, forecast)
-plot_components_plotly(m, forecast)
 
+# Plotting the components of the forecasted values
+plot_components_plotly(m, forecast)
